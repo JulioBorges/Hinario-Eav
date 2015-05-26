a573965c3bfef0137a047fb10b30e19ebd869706
JulioBorges <borges.juliocezar@gmail.com>
2015-05-26 13:22:07 -0300

    Ajustes na rotina de carregamento
    ajustes no carregamento e nav-bar superior

--- wwwroot/assets/js/hinario.controllers.js
+++ wwwroot/assets/js/hinario.controllers.js
@@ -13,12 +13,12 @@ angular.module('hinario').controller('Home', function ($scope, $sce, $http) {
 
     $scope.ButtonClick = function (valor) {
 
-        if ($scope.hino_selecionado.length == 0){
+        if ($scope.hino_selecionado.length == 0) {
             $scope.hino_selecionado = valor.toString();
             return;
         }
 
-        var prim_letra = $scope.hino_selecionado.substring(0,1);
+        var prim_letra = $scope.hino_selecionado.substring(0, 1);
         var tamanho_maximo = 3;
         if (prim_letra == 'S' || prim_letra == 'C') {
             tamanho_maximo = 4;
@@ -29,14 +29,14 @@ angular.module('hinario').controller('Home', function ($scope, $sce, $http) {
     };
 
     $scope.SelecionaHino = function (tipoHino) {
-        if ($scope.hino_selecionado.length == 0){
+        if ($scope.hino_selecionado.length == 0) {
             $scope.hino_selecionado = tipoHino;
             return;
         }
 
         var hino_temp = $scope.hino_selecionado;
-        if ($scope.hino_selecionado.substring(0,1) == 'S' ||
-            $scope.hino_selecionado.substring(0,1) == 'C'){
+        if ($scope.hino_selecionado.substring(0, 1) == 'S' ||
+            $scope.hino_selecionado.substring(0, 1) == 'C') {
             hino_temp = $scope.hino_selecionado.substr(1);
         }
 
@@ -48,14 +48,21 @@ angular.module('hinario').controller('Home', function ($scope, $sce, $http) {
     };
 
     $scope.Pesquisar = function () {
-        $scope.hinos = SearchJson($scope.hinos_origem, 'number', $scope.hino_selecionado);
-        if ($scope.hinos.length == 0){
-            Materialize.toast('Nenhum hino encontrado');
-        }
-        else{
-            $scope.pesquisando = false;
-            $scope.exibindo = true;
-        }
+        $scope.carregando = true;
+        setTimeout(function () {
+            $scope.hinos = SearchJson($scope.hinos_origem, 'number', $scope.hino_selecionado);
+            if ($scope.hinos.length == 0) {
+                $scope.carregando = false;
+                $scope.$digest();
+                Materialize.toast('Nenhum hino encontrado', 3000);
+            }
+            else {
+                $scope.pesquisando = false;
+                $scope.exibindo = true;
+                $scope.carregando = false;
+                $scope.$digest();
+            }
+        }, 2000);
     };
 
     $scope.HabilitaPesquisa = function () {
@@ -63,23 +70,23 @@ angular.module('hinario').controller('Home', function ($scope, $sce, $http) {
         $scope.hino_selecionado = '';
     };
 
-    $scope.FormatHtml = function (hinoTemp){
+    $scope.FormatHtml = function (hinoTemp) {
         return $sce.trustAsHtml(hinoTemp);
     };
 
-    function loadData(){
+    function loadData() {
         $http.get('data/hymns.json')
             .success(function (data) {
                 $scope.carregando = true;
                 // Mockando
-                /*setTimeout(function(){*/
-                $scope.hinos_origem = angular.fromJson(data);
-                $scope.carregando = false;
-                $scope.$digest();
-                /*}, 2000);*/
+                setTimeout(function () {
+                    $scope.hinos_origem = angular.fromJson(data);
+                    $scope.carregando = false;
+                    $scope.$digest();
+                }, 1500);
             }).catch(function () {
                 $scope.carregando = false;
-                Materialize.toast('Falha ao recuperar os dados', 4000);
+                Materialize.toast('Falha ao recuperar os dados', 3000);
             });
     }
 
@@ -93,9 +100,9 @@ angular.module('hinario').controller('Home', function ($scope, $sce, $http) {
             //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
             if (i == key && obj[i] == val || i == key && val == '') { //
                 objects.push(obj);
-            } else if (obj[i] == val && key == ''){
+            } else if (obj[i] == val && key == '') {
                 //only add if the object is not already in the array
-                if (objects.lastIndexOf(obj) == -1){
+                if (objects.lastIndexOf(obj) == -1) {
                     objects.push(obj);
                 }
             }
